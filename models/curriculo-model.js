9eb672bbdb6f5a9d909900885aeafa47b2843222
Sandro Reis <sandrouh@hotmail.com>
2020-04-11 15:22:03 -0300

    simples

---
+++ models/curriculo-model.js
@@ -0,0 +1,23 @@
+const data = {
+    title: 'Meu currículo',
+    name: 'Sandro Reis',
+    profession: 'Software Developer',
+    description: 'Experiência em desenvolvimento utilizando diversas tecnologias com maiores experiencias em JavaScript e frameworks JavaScript como CSS (Sass e Stylus), Git, acessibilidade e usabilidade.',
+    experience: [{
+        company: 'Embraer',
+        office: 'Estagiario em Infraestrutura de TI',
+        description: 'Trabalho na gestão de Infraestrutura de T.I na gestão dos ambientes de Servidores, Storages e Backup, utilizando conhecimentos em VMWare, Netapp entre outros, além disso utilizo meus conhecimentos em programação para buscar melhorias e soluções para área criando pequenas aplicações em Javascript e alguns scripts em Power Shell para automatizar processos'
+    },
+    {
+        company: 'UNIFEI - Universidade Federal de Itajubá',
+        office: 'Estagiário em Infraestrutura de TI',
+        description: 'Trabalhei no operacional de infraestrutura de T.I da Universidade, instalando servidores, crimpando cabos e criando scripts para soluções de problemas e rotinas nos servidores'
+    }],
+    education: [{
+        institution: 'Universidade Federal de Itajuba',
+        description: 'Sistemas de Informação'
+    }],
+    skills: ['backend', 'frontend', 'infraestrutura de T.I']
+}
+
+module.exports = data;
